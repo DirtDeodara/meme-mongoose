47ca802435ebcbec5fae5fcc8fa86289baaeaa0d
Dirt Deodara <thecrowleaps@gmail.com>
2019-07-16 11:37:22 -0700

    created schema

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 
+require('dotenv').config();
+require('./lib/utils/connect')();
 
 const app = require('./lib/app');
 
 const PORT = process.env.PORT || 9999;
 
 app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log(`Vigilantly listening on port ${PORT}`);
 });
